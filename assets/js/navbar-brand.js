8b78079f9cc99d3c839462dd4b4ee6c7f8cea916
Climate Action Darwin Technical <climateactiondarwintech@gmail.com>
2019-10-23 16:28:27 +0930

    add popping navbar brand for CAD

---
+++ assets/js/navbar-brand.js
@@ -0,0 +1,9 @@
+
+$(".collapse").on("hide.bs.collapse", function() {
+    $("a.navbar-brand").removeClass("cad-nav-menu-open");
+});
+
+$(".collapse").on("show.bs.collapse", function() {
+    $("a.navbar-brand").addClass("cad-nav-menu-open");
+});
+
