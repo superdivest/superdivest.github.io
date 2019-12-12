2c2f0c7d3638669d044e11d20b02335fd7960906
climateactiondarwin <climateactiondarwintech@gmail.com>
2019-12-12 16:34:44 +0930

    add lightbox2 to gallery template

--- assets/js/make-gallery.js
+++ assets/js/make-gallery.js
@@ -1,8 +1,19 @@
-
 var galleryContainer = $(".cad-make-gallery");
 
 galleryContainer.append('<div class="cad-gallery"></div>');
 var gallery = $(".cad-make-gallery .cad-gallery");
 
 var galleryImages = $(".cad-make-gallery img");
-galleryImages.detach().appendTo(gallery);
+galleryImages.detach().each(function (i, img) {
+    var imgSrc = $(img).attr("src");
+    var imgAlt = $(img).attr("alt");
+    var imgLink = $("<a></a>").attr("href", imgSrc)
+        .attr("data-lightbox", "gallery")
+        .attr("data-alt", imgAlt);
+    imgLink.append(img);
+    imgLink.appendTo(gallery);
+});
+
+$(document).ready(function() {
+    lightbox.option({ alwaysShowNavOnTouchDevices: true });
+});
